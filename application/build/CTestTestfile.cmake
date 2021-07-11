646685fb3aed535ab043bf2939dcab25e926561e
Asmilex <andresmm@outlook.com>
2021-07-11 19:55:52 +0200

    Añado aplicación base

---
+++ application/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Andre/Repositorios/TFG_RT_VkRay/application
+# Build directory: C:/Users/Andre/Repositorios/TFG_RT_VkRay/application/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
