6a10ab46bdccb54133e31ae1bc19001f28ca4c1d
Asmilex <andresmm@outlook.com>
2021-07-19 23:35:38 +0200

    Capítulo 7 completado

--- RT_in_one_weekend/src/main.cpp
+++ RT_in_one_weekend/src/main.cpp
@@ -2,6 +2,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "color.h"
+#include "camera.h"
 
 #include <iostream>
 
@@ -37,6 +38,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
+    const int samples_per_pixel = 100;
 
     //
     // ──────────────────────────────────────────────────────────────────── MUNDO ─────
@@ -50,14 +52,7 @@ int main() {
     // ─────────────────────────────────────────────────────────────────── CAMARA ─────
     //
 
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * aspect_ratio;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    Camera camara;
 
     //
     // ─────────────────────────────────────────────────────────────────── RENDER ─────
@@ -69,14 +64,18 @@ int main() {
         cerr << "\rLíneas restantes: " << j << ' ' << flush;
 
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i)/(image_width-1);
-            auto v = double(j)/(image_height-1);
+            color pixel_color(0, 0, 0);
 
-            Ray r (origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            for (auto s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
 
-            color pixel_color = ray_color(r, world);
+                Ray r = camara.get_ray(u, v);
 
-            write_color(std::cout, pixel_color);
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
--- RT_in_one_weekend/src/utilities.h
+++ RT_in_one_weekend/src/utilities.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 //
 // ────────────────────────────────────────────────────────── HEADERS COMUNES ─────
@@ -38,5 +39,16 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180;
 }
 
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
 
 #endif
