6dd1cf8f6e3bbba696986d9460e90c483b761def
Asmilex <andresmm@outlook.com>
2021-07-22 17:48:34 +0200

    Capítulo 4 completado

--- RT_in_one_weekend/src/random.h
+++ RT_in_one_weekend/src/random.h
@@ -13,4 +13,8 @@ inline double random_double(double min, double max) {
     return random_double() * (max - min) + min;
 }
 
+inline int random_int(int min, int max) {
+    return static_cast<int>(random_double(min, max+1));
+}
+
 #endif
