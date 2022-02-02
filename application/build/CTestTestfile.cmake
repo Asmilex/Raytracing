88bf7c5d874395ec6f2b5f22c962e42a6fcc00f1
Asmilex <andresmm@outlook.com>
2022-02-02 19:04:54 +0100

    [:fire:] Borrados archivos antiguos sin uso (#17)

--- application/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Andre/Repositorios/TFG_RT_VkRay/application
-# Build directory: C:/Users/Andre/Repositorios/TFG_RT_VkRay/application/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
